44d51fc3daae0ae49c1d0dbb3f6d3eba7edccbad
Victoire Akaniwabo <akaniwabovicky@gmail.com>
2018-03-21 19:11:54 +0200

    connection class

--- NIKI/src/niki/ConnectionClass.java
+++ NIKI/src/niki/ConnectionClass.java
@@ -14,9 +14,9 @@ public class ConnectionClass {
 	        Connection conn = null;
 	        try {
 	            Class.forName("com.mysql.jdbc.Driver");
-	           // conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/niki","second_user" ,"pass"); 
-	           // conn = DriverManager.getConnection("jdbc:mysql://197.243.18.234:3306/niki","root" ,"Msqabc!123"); 
-	            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/niki","root" ,"pass");
+	            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/niki","root" ,"Msqabc!123"); 
+	            //conn = DriverManager.getConnection("jdbc:mysql://197.243.18.234:3306/niki","root" ,"Msqabc!123"); 
+	           // conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/niki","root" ,"pass");
 	            //conn = DriverManager.getConnection("jdbc:mysql://niki-db.clpnj9wwsnxz.us-west-2.rds.amazonaws.com:3306/ebdb","niki_vicky" ,"mysqlniki");
 	            
 	        } catch (Exception e) { 
